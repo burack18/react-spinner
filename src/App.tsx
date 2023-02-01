50c260fb4c3c601dcd4ffba6edfae0af5fde9dcb
burack <omerfaruk.aksak@student.ucll.be>
2023-02-01 21:51:46 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import internal from 'stream';
 
 function App() {
+  const [deg, setDeg] = useState(90)
+  useEffect(() => {
+    setInterval(()=>{
+      setDeg(prev=>prev+1)
+    },25)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ position: 'fixed', top: '50%', left: '50%' ,opacity:'1.5'}}>
+        <div style={{ position: 'relative', width: "50px", backgroundColor: 'blue', height: '50px', borderRadius: '50%', margin: 'auto' }}>
+          <div style={{ position: 'absolute', top: '10%', left: '10%', width: "40px", backgroundColor: 'white', height: '40px', borderRadius: '50%' }} >
+            <div style={{ position: 'absolute', borderRadius: '0px 12px 10px 0px', left: '-5px', top: '15px', rotate: `${deg}deg`, height: '10px', background: 'linear-gradient(90deg,white 50%, blue 100%)', width: '50px' }}>
+            </div>
+            <div style={{ position: 'absolute', width: "40px", backgroundColor: 'white', height: '40px', borderRadius: '50%' }}>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
